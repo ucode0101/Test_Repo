0785fa88353abe2c91a15287fa072493b7127995
U-Code Academy <136019202+ucode0101@users.noreply.github.com>
2023-10-17 20:00:21 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,15 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello Git & Github");
-        System.out.println("hello");
-        System.out.println("another change");
 
-        System.out.println("Hello Git & Github");
-        System.out.println("hello");
-        System.out.println("another change");
-
-        System.out.println("Hello Git & Github");
-        System.out.println("hello");
-        System.out.println("another change");
     }
 }
